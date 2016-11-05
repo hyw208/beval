dc3ad7f785108975f41f6339d3b168d6b5a02555
hyw208 <hyw208@gmail.com>
2016-11-05 17:13:57 -0400

    first commit

---
+++ schema.py
@@ -0,0 +1,18 @@
+
+DEBUG = True
+
+class Provider(object):
+    pass
+
+class ValueGroupProvider(Provider):
+    pass
+
+class MappingDataProvider(Provider):
+    pass
+
+class Schema(object):
+    """ schema defines words/jargons used to define criteria """
+    pass
+
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+
+    name = "beval",
+
+    version = "0.1.0",
+
+    author = "hyw208",
+
+    author_email = "hyw208@gmail.com",
+
+    packages = [ "lib" ],
+
+    include_package_data = True,
+
+    url = "http://pypi.python.org/pypi/beval_v010/",
+
+    description = "TBA",
+
+    install_requires = [],
+
+)
