c383eff8e23624067a0310f10c9ec91033b19de4
hyw208 <hyw208@gmail.com>
2016-11-06 14:47:37 -0500

    add and tests

--- tests/test_and.py
+++ tests/test_and.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import TestCase
-from criteria import Criteria, Ctx, Eq, Ne, True_, False_, And
+from criteria import Criteria, Ctx, Eq, Ne, True_, False_, And, MockCriteria
 
 class TestAnd( TestCase ):
     """
@@ -54,6 +54,56 @@ class TestAnd( TestCase ):
         )
         ans, err = and_( john_duke )
         self.assertTrue( ans )
+        self.assertIsNone( err )
+
+
+    def test_and_nesting_err( self ):
+        and_ = And( MockCriteria( True, KeyError( "left first" ) ), MockCriteria( True, KeyError( "right first" ) ) )
+        and_ = And( and_, MockCriteria( True, KeyError( "left 2nd" ) ) )
+        ans, err = and_( Ctx( {} ) )
+        self.assertTrue( ans )
+        self.assertIsNotNone( err )
+        self.assertEqual( err.message, "left first" )
+
+        and_ = And( MockCriteria( True, KeyError( "left first" ) ), MockCriteria( True, KeyError( "right first" ) ) )
+        and_ = And( MockCriteria( True, KeyError( "left 2nd" ) ), and_ )
+        ans, err = and_( Ctx( {} ) )
+        self.assertTrue( ans )
+        self.assertIsNotNone( err )
+        self.assertEqual( err.message, "left 2nd" )
+
+    def test_and_left_true( self ):
+        # left.true, right.true
+        and_ = And( MockCriteria( True, KeyError( "left first" ) ), MockCriteria( True, KeyError( "right first" ) ) )
+        ans, err = and_( Ctx( {} ) )
+        self.assertTrue( ans )
+        self.assertIsNotNone( err )
+        self.assertEqual( err.message, "left first" )
+
+        # left.true, right.error, fuzzy.true
+        and_ = And( True_, MockCriteria( None, KeyError( "right first" ) ) )
+        ans, err = and_( Ctx( { "fuzzy": True } ) )
+        self.assertTrue( ans )
+        self.assertIsNotNone( err )
+        self.assertEqual( err.message, "right first" )
+
+        # left.true, right.error, fuzzy.false
+        and_ = And( True_, MockCriteria( None, KeyError( "right first" ) ) )
+        ans, err = and_( Ctx( { "fuzzy": False } ) )
+        self.assertIsNone( ans )
+        self.assertIsNotNone( err )
+        self.assertEqual( err.message, "right first" )
+
+        # left.true, right.unknown, fuzzy.true
+        and_ = And( True_, MockCriteria( Criteria.UNKNOWN, None ) )
+        ans, err = and_( Ctx( { "fuzzy": True } ) )
+        self.assertTrue( ans )
+        self.assertIsNone( err )
+
+        # left.true, right.unknown, fuzzy.false
+        and_ = And( True_, MockCriteria( Criteria.UNKNOWN, None ) )
+        ans, err = and_( Ctx( { "fuzzy": False } ) )
+        self.assertEqual( ans, Criteria.UNKNOWN )
         self.assertIsNone( err )
 
 
