c3c72cbac614089bbabfa2bac08db13cf6ca218f
hyw208 <hyw208@gmail.com>
2016-11-20 13:08:53 -0500

    correct classifier

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: Apache License, Version 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
     ],
